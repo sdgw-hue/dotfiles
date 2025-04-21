5d145bde1952b0c951410ee5d1ebb0e07bdb8004
sdgw-hue <263259031+sdgw-hue@users.noreply.github.com>
2025-04-21 10:31:42 +0300

    refactor: some changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -35,29 +35,11 @@ vim.cmd([[
 ]])
 
 --Telescope
-require('telescope').load_extension('fzf')
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', ',f', builtin.find_files, {})
-vim.keymap.set('n', ',g', builtin.live_grep, {})
-
-
--- Set the clipboard to use the system clipboard
-vim.opt.clipboard = 'unnamedplus'
-
--- Use xclip instead of wl-clipboard
--- for yanking text to the system clipboard when working in a wsl
--- vim.g.clipboard = {
---   name = 'xclip',
---   copy = {
---     ['+'] = 'xclip -selection clipboard',
---     ['*'] = 'xclip -selection primary',
---   },
---   paste = {
---     ['+'] = 'xclip -selection clipboard -o',
---     ['*'] = 'xclip -selection primary -o',
---   },
---   cache_enabled = 0,
--- }
+vim.keymap.set('n', ',ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', ',fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', ',fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', ',fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
 -- Lsp server
 -- Mappings.
@@ -91,7 +73,6 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
 end
 
 local lsp_flags = {
@@ -99,21 +80,23 @@ local lsp_flags = {
   debounce_text_changes = 150,
 }
 
-require('lspconfig')['pyright'].setup{
+local lspconfig = require('lspconfig')
+lspconfig['pyright'].setup{
   on_attach = on_attach,
   flags = lsp_flags,
 }
 
-require('lspconfig')['lua_ls'].setup{
+lspconfig['lua_ls'].setup{
   on_attach = on_attach,
   flags = lsp_flags,
 }
 
-
-require('lspconfig')['texlab'].setup{
+lspconfig['texlab'].setup{
   on_attach = on_attach,
   flags = lsp_flags,
 }
+
+require('lspconfig')['ts_ls'].setup {}
 
 -- Autocomplete
 vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
--- nvim/lua/vim_plug/plugins.lua
+++ nvim/lua/vim_plug/plugins.lua
@@ -2,25 +2,26 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
 
-	-- Colorscheme
-	Plug('flazz/vim-colorschemes')
-	Plug('xolox/vim-misc')
-	Plug('xolox/vim-colorscheme-switcher')
+    -- Colorscheme
+    Plug('flazz/vim-colorschemes')
+    Plug('xolox/vim-misc')
+    Plug('xolox/vim-colorscheme-switcher')
 
-	-- Lsp
-	Plug ('neovim/nvim-lspconfig')
+    -- Lsp
+    Plug ('neovim/nvim-lspconfig')
 
-	-- Autocomplete
-	Plug('hrsh7th/nvim-cmp')
-	Plug('hrsh7th/cmp-nvim-lsp')
-	Plug('hrsh7th/cmp-path')
-	Plug('L3MON4D3/LuaSnip')
-	Plug('hrsh7th/cmp-buffer')
-	Plug('saadparwaiz1/cmp_luasnip')
+    -- Autocomplete
+    Plug('hrsh7th/nvim-cmp')
+    Plug('hrsh7th/cmp-nvim-lsp')
+    Plug('hrsh7th/cmp-path')
+    Plug('L3MON4D3/LuaSnip')
+    Plug('hrsh7th/cmp-buffer')
+    Plug('saadparwaiz1/cmp_luasnip')
 
     --Telescope
     Plug('nvim-lua/plenary.nvim')
-    Plug('nvim-telescope/telescope.nvim', { tag = '0.1.8' })
+    Plug('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.8' })
     Plug('nvim-telescope/telescope-fzf-native.nvim', { ['do'] = 'make' })
+
 vim.call('plug#end')
 
